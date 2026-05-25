5a6afb66020caa63771f10b53259973e83bfc177
Ahmad Nashwan - WSL <ahmad.nashwan.07@gmail.com>
2026-05-26 02:31:40 +0300

    Add global variable for signals

--- src/signals/sig_handlers.c
+++ src/signals/sig_handlers.c
@@ -3,14 +3,14 @@
 void    sigint_hdoc_handler(int sig)
 {
     (void) sig;
-    g_signaled = 130;
+    g_signal = 130;
     write(STDOUT_FILENO, "\n", 1);
 }
 
 void    sigint_interactive_handler(int sig)
 {
     (void)sig;
-    g_signaled = 130;
+    g_signal = 130;
     write(STDOUT_FILENO, "\n", 1);
     rl_on_new_line();
     rl_replace_line("", 0);
